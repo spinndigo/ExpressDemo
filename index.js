6798c063d68e561feb0139e436e1e4c19be52ae5
Joseph Spinosa <spinosajoseph@gmail.com>
2023-09-11 17:23:43 -0400

    add handlebar views

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var app = express();
 
+app.disable('x-powered-by');
+
+app.engine('handlebars' , handlebars.engine);
+app.set('view engine' , 'handlebars');
+
 app.set('port' , process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // use the public folder to serve content
+
 app.get('/' , function(req , res){
-    res.send('Express works');
+    res.render('home');
 } );
+
+app.get('/about' , function(req , res) {
+    res.render('about');
+});
 
 app.listen(app.get("port") ,function(){
     console.log("Express started, press Ctrl-C to terminate");
