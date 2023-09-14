a7e6732c2f9815af8ba78dd99b5c92d36356ecea
Joseph Spinosa <spinosajoseph@gmail.com>
2023-09-14 14:11:58 -0400

    file submit and cookies

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ app.get('/about' , function(_req , res) {
     res.render('about');
 });
 
+app.get('/file-upload' , function(_req , res) {
+    var now = new Date();
+
+    res.render('file-upload', {year: now.getFullYear() , month: now.getMonth()});
+});
+
+app.post('/file-upload/:year/:month' , function(req , res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, file){
+        if(err) res.redirect(303, '/error');
+        console.log('Received File');
+        console.log(file);
+        res.redirect(303, '/thankyou');
+    })
+});
+
 app.get('/contact' , function(_req , res) {
     res.render('contact', {csrf: ' csrf token here'});
 });
@@ -40,6 +56,20 @@ app.get('/thankyou' , function(_req , res) {
     res.render('thankyou');
 });
 
+app.get('/cookie' , function(req , res){
+    res.cookie('username' , 'joe spin' , {expires: new Date() + 9999}).send('username has value : joe spin');
+});
+
+app.get('/listcookies' , function(req, res){
+    console.log('cookies: ' , req.cookies);
+    res.send('Check console');
+});
+
+app.get('/deletecookie' , function(req, res){
+    res.clearCookie('username');
+    res.send('Username cookie deleted');
+})
+
 app.use(function(req, _res, next){
     console.log("Looking for url: " + req.url);
     next();
@@ -49,7 +79,7 @@ app.use(function(err, _req, _res, _next){
     console.log("Error: " + err.message);
 })
 
-app.use(function(req, res){
+app.use(function(_req, res){
     res.type('text/html');
     res.status(404);
     res.render('404');
